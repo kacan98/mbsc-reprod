3526a8501c01849d61eaa40aa62eaac4d0e48dba
kacan98 <kacan@dynaway.com>
2024-10-21 10:45:24 +0200

    Reproduce view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
-import { MbscModule } from '@mobiscroll/angular';
+import {
+  MbscCalendarEvent,
+  MbscEventcalendarView,
+  MbscModule,
+} from '@mobiscroll/angular';
 import { FormsModule } from '@angular/forms';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,8 +12,37 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [MbscModule, FormsModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.sass',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'mbsc-reprod';
+  view: MbscEventcalendarView = {
+    timeline: {
+      type: 'week',
+      startDay: 1,
+      endDay: 5,
+      eventList: true,
+      eventHeight: 'variable',
+    },
+  };
+  boardCards?: MbscCalendarEvent[];
+
+  // constructor() {
+  //   this.boardCards = this.getCards();
+  // }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.boardCards = this.getCards();
+    }, 2000);
+  }
+
+  getCards(): MbscCalendarEvent[] {
+    return [
+      {
+        date: new Date(),
+        title: 'Event 1',
+      },
+    ];
+  }
 }
